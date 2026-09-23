0d827236a3b224c2a123f5532821c4f99d43da4d
Felipe Alves <felwalter70@gmail.com>
2026-09-23 14:46:41 -0300

    Modal Stack Implementation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
 import { MainMenu } from "./pages/MainMenu/MainMenu";
 import { AppLayout } from "./shared/layouts/AppLayout";
-import { BrowserRouter, Routes, Route } from 'react-router';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 
 export function App() {
     return (
         <BrowserRouter>
             <AppLayout>
                 <Routes>
-                    <Route path='/' element={<MainMenu />} />
+                    <Route path='/' element={<Navigate to='/mainMenu'/>} />
+                    <Route path='/mainMenu' element={<MainMenu />}/>
                     <Route path='*' element={<>Page not found! 404</>} />
                 </Routes>
             </AppLayout>
